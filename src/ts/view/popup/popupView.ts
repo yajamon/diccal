6e66769a33ae8cbcb77f1b0419fcb97c30899a42
yajamon <yajamon.tatsuki@gmail.com>
2015-04-17 21:45:06 +0900

    現在開いているwindow一覧を表示する

--- src/ts/view/popup/popupView.ts
+++ src/ts/view/popup/popupView.ts
@@ -11,6 +11,16 @@ module Diccal {
             messageTpl.set("value", "hello Diccal");
             result = messageTpl.render();
             $target.append(result);
+
+            var prms = new Promise( (resolve:(data:any)=>void, reject:(data:any)=>void) => {
+                chrome.windows.getAll({"populate": true}, resolve);
+            });
+            var windowsTpl = new Diccal.Template("popup/windows");
+            prms.then( (windows:chrome.windows.Window[]) => {
+                console.log(windows);
+                windowsTpl.set("windows", windows);
+                $target.append(windowsTpl.render());
+            });
         }
     }
 }
