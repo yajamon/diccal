7fd02de5d57056929e8ff7fc44ccee392c6aa243
yajamon <yajamon.tatsuki@gmail.com>
2015-04-17 17:25:38 +0900

    popup.htmlの描画を、popupVIewに一任するように修正

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,9 +1,6 @@
-window.onload = function () {
-    var template = Diccal.templates.popup.message;
-    var param = {
-        value: "hello world!!"
-    };
-    var html = template(param);
-    console.log(html);
-    document.body.innerHTML = html;
-}
+$(function() {
+    var popupView = new Diccal.PopupView();
+    $('#wrapper').append(
+        popupView.render()
+    );
+});
