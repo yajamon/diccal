0600ab573ca28de31f7681bfea8b38d750072b40
yajamon <yajamon.tatsuki@gmail.com>
2015-04-17 23:35:59 +0900

    calibration.html: show windowId

--- assets/js/calibration.js
+++ assets/js/calibration.js
@@ -1,5 +1,8 @@
 $(function() {
     var $wrapper = $('#wrapper');
 
+    var util = Diccal.Util;
+    var param = util.parseQueryString(util.getQueryString());
 
+    $wrapper.append('<p>Window ID:'+param['windowId']+'</p>');
 });
