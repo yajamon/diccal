c612f4c4adc36787f318f6e0e48494b9aa07002a
yajamon <yajamon.tatsuki@gmail.com>
2015-04-20 23:08:18 +0900

    refactoring

--- src/ts/view/calibration/calibrationView.ts
+++ src/ts/view/calibration/calibrationView.ts
@@ -7,8 +7,8 @@ module Diccal {
 
         public render($target:JQuery) : void {
             var util = Diccal.Util;
-            var param = util.parseQueryString(util.getQueryString());
-            var windowId = parseInt(param["windowId"], 10);
+
+            var windowId = util.getQueryString();
 
             $target.append('<p>Window ID:'+windowId+'</p>');
 
