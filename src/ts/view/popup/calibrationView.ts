73c4065c444a03123dc1ebc9fa8bfded353f4b5f
yajamon <yajamon.tatsuki@gmail.com>
2015-04-20 14:44:53 +0900

    Parse windowId(queryString) to number

--- src/ts/view/popup/calibrationView.ts
+++ src/ts/view/popup/calibrationView.ts
@@ -7,8 +7,10 @@ module Diccal {
         public render($target:JQuery) : void {
             var util = Diccal.Util;
             var param = util.parseQueryString(util.getQueryString());
+            var windowId = parseInt(param["windowId"], 10);
 
-            $target.append('<p>Window ID:'+param['windowId']+'</p>');
+            $target.append('<p>Window ID:'+windowId+'</p>');
+
         }
     }
 }
