c846f01ac3b725d1ec9f3864561d433108b2c90f
yajamon <yajamon.tatsuki@gmail.com>
2015-04-22 19:37:54 +0900

    Add method: domain/Tab#executeScript

--- src/ts/domain/window/tab.ts
+++ src/ts/domain/window/tab.ts
@@ -15,5 +15,14 @@ module Diccal {
                 return new Tab(tabs[0].id);
             }).then(callback);
         }
+
+         public executeScript(code:string, callback:any) : void {
+             new Promise((resolve:(data:any)=>void, reject:(data:any)=>void) => {
+                var details:chrome.tabs.InjectDetails = {
+                    "code": code,
+                };
+                chrome.tabs.executeScript(this.tabId, details, resolve);
+            }).then(callback);
+         }
     }
 }
