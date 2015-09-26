dad470c2ba5a503e32d083b667e89833523364a7
yajamon <yajamon.tatsuki@gmail.com>
2015-09-27 06:02:32 +0900

    implement long push compress width

--- assets/js/calibration.js
+++ assets/js/calibration.js
@@ -8,10 +8,37 @@ $(function() {
 
     var windowManager = new Diccal.Window(windowId);
 
+    var longPushIntervalId = null;
+    var longPushStartTime = null;
+
     $wrapper.on('click', '.compressWidth', function(event) {
         event.preventDefault();
         /* Act on the event */
         windowManager.compressWidth();
+    });
+    $wrapper.on('mousedown', '.compressWidth', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        if (longPushIntervalId) {
+            clearInterval(longPushIntervalId);
+            longPushIntervalId = null;
+            longPushStartTime = null;
+        }
+        longPushStartTime = new Date().getTime();
+        longPushIntervalId = setInterval(function () {
+            var now = new Date().getTime();
+            if (now - longPushStartTime < 500) {
+                return;
+            }
+            windowManager.compressWidth();
+        }, 1000/30);
+    });
+    $wrapper.on('mouseup mouseout', '.compressWidth', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        clearInterval(longPushIntervalId);
+        longPushIntervalId = null;
+        longPushStartTime = null;
     });
     $wrapper.on('click', '.expandWidth', function(event) {
         event.preventDefault();
